9b508004b6a8b084753e1e7e7e6f095f741a2137
CJones001 <147557670+CJones001@users.noreply.github.com>
2025-01-10 21:01:19 -0700

    Commented the code

--- youtube.py
+++ youtube.py
@@ -5,5 +5,14 @@ from tkinter import filedialog
 def download_video(url, save_path):
     try:
         yt = YouTube(url)
+        # yt.streams accesses all available YouTube streams that match the url
+        # progressive=True filters streams with both audio and video
+        # file_extension="mp4" ensures the video format is MP4
         streams = yt.streams.filter(progressive=True, file_extension="mp4")
-        
+        # Retrieve the highest-quality resolution video
+        highest_res_stream = streams.get_highest_resolution()
+        # Download the selected video stream to the file save_path
+        highest_res_stream.download(output_path=save_path)
+
+    except Exception as e:
+        print(e)
